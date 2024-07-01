40199fbc264fa1cb1c43f7664e7fad053bba0e84
Stephan Sahm <stephan.sahm@jolin.io>
2024-07-01 12:52:37 +0200

    deciding on main packages
    something like PlutoGrid, but with reactivity, should also become part of this

--- src/Jolin.jl
+++ src/Jolin.jl
@@ -1,5 +1,12 @@
 module Jolin
+using Reexport
 
-# Write your package code here.
+@reexport using JolinPluto
+@reexport using PlutoUI
+@reexport using PlutoPlotly
+
+# interactive access to DataFrames with Pluto Reactivity on Events would be awesome (selecting rows, filtering rows, ...)
+# https://github.com/lungben/PlutoGrid.jl ? 
+# https://github.com/JuliaComputing/TableView.jl looks slightly more general
 
 end
