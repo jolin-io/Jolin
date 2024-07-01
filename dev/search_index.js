50a4219e7f3231c94cd63cba3650b79067879daa
Documenter.jl <documenter@juliadocs.github.io>
2024-07-01 09:28:20 +0000

    build based on 264e2fe

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Jolin","category":"page"},{"location":"#Jolin","page":"Home","title":"Jolin","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Jolin.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Jolin]","category":"page"}]
+}
