aeea6e164116cfe942d53b1c9c3a3d5976870f62
Stephan Sahm <stephan.sahm@jolin.io>
2024-08-12 12:13:20 +0200

    bringing jolin cloud specific parts from JolinPluto to here

--- src/Jolin.jl
+++ src/Jolin.jl
@@ -2,11 +2,16 @@ module Jolin
 using Reexport
 
 @reexport using JolinPluto
+export JolinPluto
 @reexport using PlutoUI
+export PlutoUI
 @reexport using PlutoPlotly
+export PlutoPlotly
 
 # interactive access to DataFrames with Pluto Reactivity on Events would be awesome (selecting rows, filtering rows, ...)
-# https://github.com/lungben/PlutoGrid.jl ? 
-# https://github.com/JuliaComputing/TableView.jl looks slightly more general
+# https://github.com/JuliaComputing/TableView.jl looks like the way to go, it is even still maintained - however this does not run in Pluto...
+# https://github.com/lungben/PlutoGrid.jl - is not even registered, but uses the same underlying ag-grid javascript module and works in Pluto
+
+include("authentication.jl")
 
 end
